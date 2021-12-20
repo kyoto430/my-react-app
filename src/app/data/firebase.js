1fbb6bd6484ceb420b3402015ab6895445dcca51
kyoto430 <nateriver345@gmail.com>
2021-12-20 16:31:34 +0300

    add feed section and firebase

---
+++ src/app/data/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCvbCOkXFKmC9njxa9njfhg5vibawapQd0',
+  authDomain: 'my-react-app-c39a1.firebaseapp.com',
+  projectId: 'my-react-app-c39a1',
+  storageBucket: 'my-react-app-c39a1.appspot.com',
+  messagingSenderId: '526237422118',
+  appId: '1:526237422118:web:ccfe15eb14095b91a34ce7',
+}
